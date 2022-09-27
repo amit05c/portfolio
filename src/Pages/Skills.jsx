56fd751d91ef8776066f1a707ec306e828bfd72b
amig05c <amitghosh0595c@gmail.com>
2022-09-28 01:27:30 +0530

    changes in skills icons

--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -28,7 +28,7 @@ const Skills = () => {
       <h1 data-aos={width > 920 ? "fade-up" : ""} style={{marginTop:"8rem"}}>Skills</h1>
       <div className={styles.skills_lists}>
         <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
-          <img src={reactLogo} alt="react_logo" width={"100px"}  />
+          <img src={reactLogo} alt="react_logo" width={"70%"}  />
         </div>
 
         <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
@@ -36,7 +36,7 @@ const Skills = () => {
             src={Redux}
             alt={"redux_logo"}
             style={{ backgroundColor: "whitesmoke" }}
-            width={"100px"}
+            width={"80%"}
           />
         </div>
 
@@ -47,7 +47,7 @@ const Skills = () => {
             src={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png"}
             alt={"redux_logo"}
             style={{ backgroundColor: "whitesmoke" }}
-            width={"50%"}
+            width={"80%"}
           />
         </div>
 
@@ -58,20 +58,20 @@ const Skills = () => {
             src={"https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png"}
             alt={"redux_logo"}
             style={{ backgroundColor: "whitesmoke" }}
-            width={"50%"}
+            width={"70%"}
           />
         </div>
 
 
         <div className={styles.logo} data-aos={width > 920 ? "fade-right" : ""}>
-          <img src={javascript} alt={"javascript_logo"} width={"85px"} />
+          <img src={javascript} alt={"javascript_logo"} width={"50%"} />
         </div>
 
         <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
           <img
             src={cssLogo}
             alt={"css_logo"}
-            width={"50%"}
+            width={"70%"}
           
           />
         </div>
@@ -80,7 +80,7 @@ const Skills = () => {
           <img
             src={chakraUi}
             alt={"chakra_logo"}
-            width={"50%"}
+            width={"80%"}
             
           />
         </div>
@@ -88,7 +88,7 @@ const Skills = () => {
           <img
             src={bootstrap}
             alt={"bootstrap_logo"}
-            width={"50%"}
+            width={"70%"}
             
           />
         </div>
