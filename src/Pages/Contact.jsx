38e5728db8c4b1c75fd4e3517f7f2c11117efa10
amig05c <amitghosh0595c@gmail.com>
2022-09-27 20:40:55 +0530

    responsiveness changes

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,38 +1,75 @@
-import React, { useContext, useEffect } from 'react'
-import styles from "../Styles/contact.module.css"
-import { AiOutlineMail } from 'react-icons/ai';
-import { BsTelephoneInbound } from 'react-icons/bs';
-import { BsLinkedin } from 'react-icons/bs';
-import { FiTwitter } from 'react-icons/fi';
-import { AiFillGithub } from 'react-icons/ai';
-import Aos from 'aos'
-import "aos/dist/aos.css"
-import { ScreenContext } from '../screenContext/ScreenContext'
+import React, { useContext, useEffect } from "react";
+import styles from "../Styles/contact.module.css";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsTelephoneInbound } from "react-icons/bs";
+import { BsLinkedin } from "react-icons/bs";
+import { FiTwitter } from "react-icons/fi";
+import { AiFillGithub } from "react-icons/ai";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { ScreenContext } from "../screenContext/ScreenContext";
 
 const Contact = () => {
-  const {windowDimensions}= useContext(ScreenContext)
-  const width=windowDimensions.width
+  const { windowDimensions } = useContext(ScreenContext);
+  const width = windowDimensions.width;
 
-  useEffect(()=>{
-    Aos.init({duration:1000})
-  },[])
-
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
 
   return (
-    <div className={styles.contact} id={'contact'}>
-        <h1 data-aos={width>920 ? "fade-up" : ""} >Contacts</h1>
-        <div className={styles.contact_list}>
-            <div data-aos={width>920 ? "fade-left" : ""} ><a className={styles.logo} href="https://github.com/amit05c" target={"_balnk"}><AiFillGithub fontSize={"larger"}/></a></div>
-            <div data-aos={width>920 ? "fade-left" : ""} ><a className={styles.logo} href="mailto:amitghosh0595c@gmail.com" target={"_balnk"}><AiOutlineMail fontSize={"larger"}/></a></div>
-            <div data-aos={width>920 ? "fade-left" : ""} className={styles.logo} onClick={()=>alert(`Phone no is 7551021361`)}><BsTelephoneInbound fontSize={"larger"}/></div>
-            <div data-aos={width>920 ? "fade-right" : ""}><a className={styles.logo} href="https://www.linkedin.com/in/amit-ghosh-0b6807228/" target={"_blank"}><BsLinkedin fontSize={"larger"} /></a></div>
-            <div data-aos={width>920 ? "fade-right" : ""}><a className={styles.logo} href="https://twitter.com/Amit05C" target={"_blank"}><FiTwitter fontSize={"larger"}/></a></div>
+    <div className={styles.contact} id={"contact"}>
+      <h1 data-aos={width > 920 ? "fade-up" : ""}>Contacts</h1>
+      <div className={styles.contact_list}>
+        <div data-aos={width > 920 ? "fade-left" : ""}>
+          <a
+            className={styles.logo}
+            href="https://github.com/amit05c"
+            target={"_balnk"}
+          >
+            <AiFillGithub fontSize={"larger"} />
+          </a>
         </div>
-        <div className={styles.copy}>
-          <h3>© 2022 copyright all right reserved</h3>
+        <div data-aos={width > 920 ? "fade-left" : ""}>
+          <a
+            className={styles.logo}
+            href="mailto:amitghosh0595c@gmail.com"
+            target={"_balnk"}
+          >
+            <AiOutlineMail fontSize={"larger"} />
+          </a>
         </div>
+        <div
+          data-aos={width > 920 ? "fade-left" : ""}
+          // className={styles.logo}
+          onClick={() => alert(`Phone no is 7551021361`)}
+        >
+          <BsTelephoneInbound className={styles.logo} fontSize={"larger"} />
+        </div>
+        <div data-aos={width > 920 ? "fade-right" : ""}>
+          <a
+            className={styles.logo}
+            href="https://www.linkedin.com/in/amit-ghosh-0b6807228/"
+            target={"_blank"}
+          >
+            <BsLinkedin fontSize={"larger"} />
+          </a>
+        </div>
+        <div data-aos={width > 920 ? "fade-right" : ""}>
+          <a
+            className={styles.logo}
+            href="https://twitter.com/Amit05C"
+            target={"_blank"}
+          >
+            <FiTwitter fontSize={"larger"} />
+          </a>
+        </div>
+      </div>
+      <div className={styles.copy}>
+        <h3>© 2022 copyright all right reserved</h3>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/Pages/Skills.jsx
+++ src/Pages/Skills.jsx
@@ -42,25 +42,25 @@ const Skills = () => {
 
 
 
-        {/* <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
+        <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
           <img
             src={"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Node.js_logo.svg/1280px-Node.js_logo.svg.png"}
             alt={"redux_logo"}
             style={{ backgroundColor: "whitesmoke" }}
-            width={"100%"}
+            width={"50%"}
           />
-        </div> */}
+        </div>
 
 
 
-        {/* <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
+        <div className={styles.logo} data-aos={width > 920 ? "fade-left" : ""}>
           <img
             src={"https://w7.pngwing.com/pngs/925/447/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo.png"}
             alt={"redux_logo"}
             style={{ backgroundColor: "whitesmoke" }}
-            width={"100%"}
+            width={"50%"}
           />
-        </div> */}
+        </div>
 
 
         <div className={styles.logo} data-aos={width > 920 ? "fade-right" : ""}>
@@ -71,7 +71,7 @@ const Skills = () => {
           <img
             src={cssLogo}
             alt={"css_logo"}
-            width={"90px"}
+            width={"50%"}
           
           />
         </div>
@@ -80,7 +80,7 @@ const Skills = () => {
           <img
             src={chakraUi}
             alt={"chakra_logo"}
-            width={"100px"}
+            width={"50%"}
             
           />
         </div>
@@ -88,7 +88,7 @@ const Skills = () => {
           <img
             src={bootstrap}
             alt={"bootstrap_logo"}
-            width={"90px"}
+            width={"50%"}
             
           />
         </div>
